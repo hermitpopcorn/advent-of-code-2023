88d426b2fa5464a0ddd39afef154814ad4eacf86
Dani Nugraha <dani.hmng@gmail.com>
2023-12-04 20:07:06 +0700

    added day04 part 1 solution

---
+++ day04/src/bin/part1.rs
@@ -0,0 +1,83 @@
+use std::collections::HashSet;
+
+fn main() {
+    let inputs = std::fs::read_to_string("input/real.txt").unwrap();
+    let inputs: Vec<&str> = inputs.split("\n").collect();
+
+    let mut values = vec![];
+    for input in inputs {
+        if input.len() < 1 {
+            continue;
+        }
+
+        let points = parse_card(input);
+        values.push(points);
+
+        println!("{} <- {}", points, input);
+    }
+
+    let answer: u32 = values.iter().sum();
+    println!("Answer: {}", answer);
+}
+
+fn parse_card(input: &str) -> u32 {
+    let (_card_number, card_body) = get_card_number_and_body(input);
+
+    let (winning_numbers, my_numbers) = parse_card_body_into_number_vectors(&card_body);
+
+    let matching_numbers = get_matching_numbers(winning_numbers, my_numbers);
+
+    if matching_numbers.len() < 1 {
+        return 0;
+    }
+
+    let base_points: u32 = 2;
+    let power: u32 = (matching_numbers.len() - 1) as u32;
+
+    base_points.pow(power)
+}
+
+fn get_card_number_and_body(input: &str) -> (u32, String) {
+    let split: Vec<&str> = input.split(": ").collect();
+
+    let card_number = parse_card_number(split.get(0).unwrap());
+    let card_body = String::from(*split.get(1).unwrap());
+
+    (card_number, card_body)
+}
+
+fn parse_card_number(input: &str) -> u32 {
+    let without_first_five = &input[5..];
+    let numeric_string = String::from(without_first_five);
+    numeric_string.trim().parse().unwrap()
+}
+
+fn parse_card_body_into_number_vectors(input: &str) -> (Vec<u32>, Vec<u32>) {
+    let split_body: Vec<&str> = input.split(" | ").collect();
+
+    let winning_numbers = parse_string_of_numbers_into_number_vector(split_body.get(0).unwrap());
+    let my_numbers = parse_string_of_numbers_into_number_vector(split_body.get(1).unwrap());
+
+    (winning_numbers, my_numbers)
+}
+
+fn parse_string_of_numbers_into_number_vector(input: &str) -> Vec<u32> {
+    let split: Vec<&str> = input.split(" ").collect();
+
+    let mut numbers = vec![];
+    for number in split {
+        if number.len() < 1 {
+            continue;
+        }
+
+        let numeric = number.parse().unwrap();
+        numbers.push(numeric);
+    }
+
+    numbers
+}
+
+fn get_matching_numbers(winning_numbers: Vec<u32>, my_numbers: Vec<u32>) -> Vec<u32> {
+    let set: HashSet<u32> = winning_numbers.into_iter().collect();
+    my_numbers.into_iter().filter(|n| set.contains(n)).collect()
+}
