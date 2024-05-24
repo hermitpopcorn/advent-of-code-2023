43affbeed360c1eabe2f4e1e6660edcfac3a39e7
Dani Nugraha <dani.hmng@gmail.com>
2024-05-24 00:29:36 +0000

    changed Part 1 main function to take an argument for input file

--- day18/part1.c
+++ day18/part1.c
@@ -2,9 +2,17 @@
 #include "parser.h"
 #include <stdio.h>
 
-int main()
+int main(int argc, char* argv[])
 {
-	struct DigInstructions parseResult = ParseFile("input/real.txt");
+	char* filename = "input/example.txt";
+	if (argc >= 2) {
+		filename = argv[1];
+	}
+	struct DigInstructions parseResult = ParseFile(filename);
+
+	if (parseResult.size < 1) {
+		printf("Could not find dig instructions.\n");
+	}
 
 	for (int i = 0; i < parseResult.size; i++) {
 		struct DigInstruction* instruction = &parseResult.instructions[i];
